84914f426e5495a53e0c1ba026d957915c18dcc4
student <student@158.197.35.239>
2021-09-28 12:41:29 +0200

    v

--- src/paz1c_vitkovska/prva.java
+++ src/paz1c_vitkovska/prva.java
@@ -2,7 +2,14 @@ package paz1c_vitkovska;
 
 public class prva {
 	public static void main(String[] args) {
-		
+		System.out.println("Prvy vypis");
+		System.out.println("Autor : Git je celkom hrozny.");
+        System.out.println("Kamarat:Git je celkom fajn. ");		
+        String n = "nastya";
+        String v = "vasya";
+        String l = "lubov";
+        System.out.println(n+"+"+v+" = "+l);
+        
 	}
 
 }
